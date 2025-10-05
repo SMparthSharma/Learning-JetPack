2651c507c4fe8ecb9d03006029e7826f4fe6cfa8
SMparthSharma <darkcodeing0@gmail.com>
2025-10-05 20:53:50 +0530

    day5 of learning

---
+++ app/src/main/java/com/example/firstjpcproject/3_LayoutBasics/Layouts.kt
@@ -0,0 +1,2 @@
+package com.example.firstjpcproject.`3_LayoutBasics`
+
--- app/src/main/java/com/example/firstjpcproject/MainActivity.kt
+++ app/src/main/java/com/example/firstjpcproject/MainActivity.kt
@@ -4,17 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.firstjpcproject.`1_FundamentalConcept`.AccessRes
-import com.example.firstjpcproject.`1_FundamentalConcept`.Greeting
-import com.example.firstjpcproject.`2_CoreConcept`.SimpleText
-import com.example.firstjpcproject.`2_CoreConcept`.TextInput
+import com.example.firstjpcproject.`2_CoreConcept`.AnnotationText
+import com.example.firstjpcproject.`2_CoreConcept`.ButtonFile
 import com.example.firstjpcproject.ui.theme.FirstJPCProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +14,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FirstJPCProjectTheme {
-                TextInput()
+                ButtonFile()
             }
         }
     }
